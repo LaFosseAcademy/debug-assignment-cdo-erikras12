9fc0b0f3b0744e0b61a35a75dc9b9d4202e14ff9
erikras12 <erikras12@hotmail.com>
2025-02-20 09:28:23 +0000

    add aws_ami data rather than hardcoding

--- terraform/data.tf
+++ terraform/data.tf
@@ -2,6 +2,23 @@ resource "aws_default_vpc" "default" {
 
 }
 
+data "aws_ami" "aws_linux_2023_ami_latest" {
+    most_recent = true
+    owners = ["amazon"]
+    filter {
+        name = "name"
+        values = ["al2023-ami-2023*"]
+    }
+    filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+    }
+    filter {
+        name = "architecture"
+        values = ["x86_64"]
+    }
+}
+
 data "aws_subnets" "default_" {
     filter {
         name = "vpc-id"
